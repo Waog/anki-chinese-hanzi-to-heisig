acc231e8d530b0d7cd6736f46620bcb9cbc46744
Oliver Stadie <hello@waog.net>
2017-03-07 16:07:44 +0100

    INITIAL COMMIT heisiginfo with modified copyrights
    original version of heisiginfo with modified copy rights

---
+++ heisiginfo/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Copyright: Ian Worthington <worthy.vii@gmail.com> (author of the original 'heisiginfo' plugin)
+# Copyright: Oliver Stadie <oliver.stadie@gmail.com> (author of the 'anki-chinese-hanzi-to-heisig' plugin)
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
---
+++ hinfo.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright: Ian Worthington <worthy.vii@gmail.com> (author of the original 'heisiginfo' plugin)
+# Copyright: Oliver Stadie <oliver.stadie@gmail.com> (author of the 'anki-chinese-hanzi-to-heisig' plugin)
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+
+import heisiginfo.addheisiginfo
